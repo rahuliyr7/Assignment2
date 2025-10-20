d07108ae85d6d3a280370926066fddd8f5317a8a
rahuliyr7 <rti@njit.edu>
2025-10-19 20:50:45 -0400

    Add files via upload

--- reversedString.js
+++ reversedString.js
@@ -1,11 +1,11 @@
-document.getElementByID("reversedStringForm").addEventListener("submit", function(event){
+document.getElementById("reversedStringForm").addEventListener("submit", function(event){
     event.preventDefault();
 
-    var inputText = document.getElementByID("input").value;
+    var inputText = document.getElementById("input").value;
     var reversed = ""
     
     for( var i = inputText.length-1; i>=0; i --) {
         reversed += inputText[i];
     }
-    document.getElementByID("reversedString").textContent = reversed;
+    document.getElementById("reversedString").textContent = reversed;
 });
